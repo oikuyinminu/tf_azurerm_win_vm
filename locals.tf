2dea6736497a1917613d0e4bbb066b76b4d15fad
Banji IKUYINMINU <i.banji@hotmail.com>
2022-03-15 21:45:36 +1100

    adjust resource group name

--- locals.tf
+++ locals.tf
@@ -1,20 +1,30 @@
 locals {
-    tags = var.optional_tags != "" ? merge(var.required_tags, var.optional_tags) : var.required_tags
-    vm_tags = var.power_management_tags.VM_StartTime != "" ? merge(local.tags, var.power_management_tags) : local.tags
+  tags    = var.optional_tags != "" ? merge(var.required_tags, var.optional_tags) : var.required_tags
+  vm_tags = var.power_management_tags.VM_StartTime != "" ? merge(local.tags, var.power_management_tags) : local.tags
 
   ########
   #Static Mapping of ENV and Regions
   ########
   env = {
-      production = "PRD"
-      preproduction = "PPD"
-      development = "DEV"
-      test = "TST"
+    "production"    = "PRD"
+    "preproduction" = "PPD"
+    "development"   = "DEV"
+    "test"          = "TST"
   }
 
-  region = {
-      eastus = "EUS1"
-      eastus2 = "EUS2"
+  reg = {
+    "eastus"  = "EUS1"
+    "eastus2" = "EUS2"
   }
+
+ vm_data_disks = flatten([for vms, disks in zipmap(range(length(var.data_disks)), var.data_disks) : [
+    for disk in disks : {
+      vm_id        = vms
+      disk_id      = disk.id
+      disk_caching = disk.caching
+      disk_lun     = disk.lun
+      disk_type    = disk.type
+    }]
+  ])
 
 }
