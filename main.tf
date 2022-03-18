fa787e1c25f713a3cabc007f2c82566f8324a94f
Banji IKUYINMINU <i.banji@hotmail.com>
2022-03-18 12:30:43 +1100

    enable remote state

--- main.tf
+++ main.tf
@@ -45,6 +45,7 @@ resource "azurerm_windows_virtual_machine" "windows_vm" {
 
   provision_vm_agent        = true
   patch_mode                = "Manual"
+  enable_automatic_updates = false
   timezone                  = var.timezone
 
   license_type = var.hub_license_type != "" ? var.hub_license_type : null
